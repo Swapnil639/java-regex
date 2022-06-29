526f332f046ad251f09484aadc7ab665885bdd06
Swapnil639 <swapnilpatil631997@gmail.com>
2022-06-29 23:45:58 +0530

    Added :Check if the password is valid or not with at least one uppercase

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -8,19 +8,22 @@ public class UserRegistration {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to user registration program using regex");
         System.out.print("Enter First Name :");
-        String firstName=scanner.next();
-        System.out.println(Pattern.matches("[A-Z][a-z]{3,}",firstName));
+        String firstName = scanner.next();
+        System.out.println(Pattern.matches("[A-Z][a-z]{3,}", firstName));
         System.out.print("Enter Last Name :");
-        String lastName=scanner.next();
-        System.out.println(Pattern.matches("[A-Z][a-z]{3,}",lastName));
+        String lastName = scanner.next();
+        System.out.println(Pattern.matches("[A-Z][a-z]{3,}", lastName));
         System.out.print("Enter Email :");
-        String email=scanner.next();
-        System.out.println(Pattern.matches("[a-z][A-Z a-z 0-9]+[@][a-z]+[.][a-z]{2,5}",email));
+        String email = scanner.next();
+        System.out.println(Pattern.matches("[a-z][A-Z a-z 0-9]+[@][a-z]+[.][a-z]{2,5}", email));
         System.out.println("Enter Mobile Number :");
-        String mobileNumber=scanner.next();
-        System.out.println(Pattern.matches("[+]\\d{2}-\\d{10}",mobileNumber));
+        String mobileNumber = scanner.next();
+        System.out.println(Pattern.matches("[+]\\d{2}-\\d{10}", mobileNumber));
         System.out.println("Enter Password :");
-        String password=scanner.next();
-        System.out.println(Pattern.matches("[A-Z a-z]{8,}",password));
+        String password = scanner.next();
+        System.out.println(Pattern.matches("[A-Z a-z]{8,}", password));
+        System.out.println("At least one Upper case in Password :");
+        String password1 = scanner.next();
+        System.out.println(Pattern.matches("[A-Z][A-Z a-z 0-9]{8,}", password1));
     }
 }

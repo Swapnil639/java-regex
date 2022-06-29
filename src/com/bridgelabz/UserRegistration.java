ec47de085cdfc2832559a45e8cb7f29264f84725
Swapnil639 <swapnilpatil631997@gmail.com>
2022-06-29 23:10:53 +0530

    Added :Ability to check the user's mobile number valid or not

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -16,5 +16,8 @@ public class UserRegistration {
         System.out.print("Enter Email :");
         String email=scanner.next();
         System.out.println(Pattern.matches("[a-z][A-Z a-z 0-9]+[@][a-z]+[.][a-z]{2,5}",email));
+        System.out.println("Enter Mobile Number :");
+        String mobileNumber=scanner.next();
+        System.out.println(Pattern.matches("[+]\\d{2}-\\d{10}",mobileNumber));
     }
 }

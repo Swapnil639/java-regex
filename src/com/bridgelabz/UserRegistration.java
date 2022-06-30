9447b686a086c2014d0b68874423778dce861b83
Swapnil639 <swapnilpatil631997@gmail.com>
2022-06-30 08:09:20 +0530

    Added :create password with 1 special character

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -16,17 +16,20 @@ public class UserRegistration {
         System.out.print("Enter Email :");
         String email = scanner.next();
         System.out.println(Pattern.matches("[a-z][A-Z a-z 0-9]+[@][a-z]+[.][a-z]{2,5}", email));
-        System.out.println("Enter Mobile Number :");
+        System.out.print("Enter Mobile Number :");
         String mobileNumber = scanner.next();
         System.out.println(Pattern.matches("[+]\\d{2}-\\d{10}", mobileNumber));
-        System.out.println("Enter Password :");
+        System.out.print("Enter Password :");
         String password = scanner.next();
         System.out.println(Pattern.matches("[A-Z a-z]{8,}", password));
-        System.out.println("At least one Upper case in Password :");
+        System.out.print("Use at least one Upper case in the Password :");
         String password1 = scanner.next();
         System.out.println(Pattern.matches("[A-Z][A-Z a-z]{8,}", password1));
-        System.out.println("At least one numeric number in the password");
+        System.out.print("use at least one numeric number in the password");
         String password2=scanner.next();
         System.out.println(Pattern.matches("[A-z a-z 0-9]{8,}[0-9]{1,}",password2));
+        System.out.print("Use at least one special character in the password  :");
+        String passWord3=scanner.next();
+        System.out.println(Pattern.matches("(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}",passWord3));
     }
 }
